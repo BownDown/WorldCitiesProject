09f2eb0d52d0ba326b7a479505e3a86ae3cdb06a
Beasting <louie.escalera@students.aie.edu.au>
2019-03-05 19:49:17 -0800

    Basic Preload
    Only got Philippines adding more later

--- WorldCitiesProject/frmCities.Designer.vb
+++ WorldCitiesProject/frmCities.Designer.vb
@@ -22,255 +22,138 @@ Partial Class frmCities
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.pnlFontStyle = New System.Windows.Forms.Panel()
-        Me.btnReset = New System.Windows.Forms.Button()
-        Me.chkBold = New System.Windows.Forms.CheckBox()
-        Me.chkItalic = New System.Windows.Forms.CheckBox()
-        Me.btnExit = New System.Windows.Forms.Button()
-        Me.lblWrittenBy = New System.Windows.Forms.Label()
+        Me.picCities = New System.Windows.Forms.PictureBox()
         Me.rtbInformation = New System.Windows.Forms.RichTextBox()
+        Me.lstCountries = New System.Windows.Forms.ListBox()
         Me.pnlCities = New System.Windows.Forms.Panel()
-        Me.radYork = New System.Windows.Forms.RadioButton()
-        Me.radCambridge = New System.Windows.Forms.RadioButton()
-        Me.radLiverpoll = New System.Windows.Forms.RadioButton()
-        Me.radBristol = New System.Windows.Forms.RadioButton()
-        Me.radManchester = New System.Windows.Forms.RadioButton()
-        Me.radQuezon = New System.Windows.Forms.RadioButton()
-        Me.Manila = New System.Windows.Forms.RadioButton()
-        Me.radIloilo = New System.Windows.Forms.RadioButton()
-        Me.radBoracay = New System.Windows.Forms.RadioButton()
-        Me.radBacolod = New System.Windows.Forms.RadioButton()
-        Me.picCity = New System.Windows.Forms.PictureBox()
-        Me.pnlFontStyle.SuspendLayout()
-        Me.pnlCities.SuspendLayout()
-        CType(Me.picCity, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlFormatControl = New System.Windows.Forms.Panel()
+        Me.btnItalic = New System.Windows.Forms.Button()
+        Me.btnUnderline = New System.Windows.Forms.Button()
+        Me.btnBold = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDesignedby = New System.Windows.Forms.Label()
+        CType(Me.picCities, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlFormatControl.SuspendLayout()
         Me.SuspendLayout()
         '
-        'pnlFontStyle
+        'picCities
         '
-        Me.pnlFontStyle.Controls.Add(Me.btnReset)
-        Me.pnlFontStyle.Controls.Add(Me.chkBold)
-        Me.pnlFontStyle.Controls.Add(Me.chkItalic)
-        Me.pnlFontStyle.Location = New System.Drawing.Point(612, 267)
-        Me.pnlFontStyle.Name = "pnlFontStyle"
-        Me.pnlFontStyle.Size = New System.Drawing.Size(88, 97)
-        Me.pnlFontStyle.TabIndex = 0
-        '
-        'btnReset
-        '
-        Me.btnReset.Location = New System.Drawing.Point(0, 63)
-        Me.btnReset.Name = "btnReset"
-        Me.btnReset.Size = New System.Drawing.Size(75, 23)
-        Me.btnReset.TabIndex = 3
-        Me.btnReset.Text = "Reset"
-        Me.btnReset.UseVisualStyleBackColor = True
-        '
-        'chkBold
-        '
-        Me.chkBold.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.chkBold.Location = New System.Drawing.Point(3, 3)
-        Me.chkBold.Name = "chkBold"
-        Me.chkBold.Size = New System.Drawing.Size(75, 24)
-        Me.chkBold.TabIndex = 1
-        Me.chkBold.Text = "Bold"
-        Me.chkBold.UseVisualStyleBackColor = True
-        '
-        'chkItalic
-        '
-        Me.chkItalic.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.chkItalic.Location = New System.Drawing.Point(3, 33)
-        Me.chkItalic.Name = "chkItalic"
-        Me.chkItalic.Size = New System.Drawing.Size(82, 24)
-        Me.chkItalic.TabIndex = 2
-        Me.chkItalic.Text = "Italic"
-        Me.chkItalic.UseVisualStyleBackColor = True
-        '
-        'btnExit
-        '
-        Me.btnExit.Location = New System.Drawing.Point(656, 526)
-        Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(75, 23)
-        Me.btnExit.TabIndex = 0
-        Me.btnExit.Text = "E&xit"
-        Me.btnExit.UseVisualStyleBackColor = True
-        '
-        'lblWrittenBy
-        '
-        Me.lblWrittenBy.Location = New System.Drawing.Point(12, 468)
-        Me.lblWrittenBy.Name = "lblWrittenBy"
-        Me.lblWrittenBy.Size = New System.Drawing.Size(100, 23)
-        Me.lblWrittenBy.TabIndex = 1
-        Me.lblWrittenBy.Text = "WrittenBy"
+        Me.picCities.Location = New System.Drawing.Point(13, 34)
+        Me.picCities.Name = "picCities"
+        Me.picCities.Size = New System.Drawing.Size(556, 217)
+        Me.picCities.TabIndex = 0
+        Me.picCities.TabStop = False
         '
         'rtbInformation
         '
-        Me.rtbInformation.Location = New System.Drawing.Point(158, 293)
+        Me.rtbInformation.Location = New System.Drawing.Point(13, 257)
         Me.rtbInformation.Name = "rtbInformation"
-        Me.rtbInformation.Size = New System.Drawing.Size(448, 256)
-        Me.rtbInformation.TabIndex = 2
+        Me.rtbInformation.Size = New System.Drawing.Size(556, 266)
+        Me.rtbInformation.TabIndex = 1
         Me.rtbInformation.Text = ""
+        '
+        'lstCountries
+        '
+        Me.lstCountries.FormattingEnabled = True
+        Me.lstCountries.Items.AddRange(New Object() {"Philippines", "United Kingdom", "Russia", "United States"})
+        Me.lstCountries.Location = New System.Drawing.Point(576, 34)
+        Me.lstCountries.Name = "lstCountries"
+        Me.lstCountries.Size = New System.Drawing.Size(196, 95)
+        Me.lstCountries.TabIndex = 2
         '
         'pnlCities
         '
-        Me.pnlCities.Controls.Add(Me.radYork)
-        Me.pnlCities.Controls.Add(Me.radCambridge)
-        Me.pnlCities.Controls.Add(Me.radLiverpoll)
-        Me.pnlCities.Controls.Add(Me.radBristol)
-        Me.pnlCities.Controls.Add(Me.radManchester)
-        Me.pnlCities.Controls.Add(Me.radQuezon)
-        Me.pnlCities.Controls.Add(Me.Manila)
-        Me.pnlCities.Controls.Add(Me.radIloilo)
-        Me.pnlCities.Controls.Add(Me.radBoracay)
-        Me.pnlCities.Controls.Add(Me.radBacolod)
-        Me.pnlCities.Location = New System.Drawing.Point(12, 12)
+        Me.pnlCities.Location = New System.Drawing.Point(576, 136)
         Me.pnlCities.Name = "pnlCities"
-        Me.pnlCities.Size = New System.Drawing.Size(140, 271)
+        Me.pnlCities.Size = New System.Drawing.Size(200, 211)
         Me.pnlCities.TabIndex = 3
         '
-        'radYork
+        'pnlFormatControl
         '
-        Me.radYork.Location = New System.Drawing.Point(4, 218)
-        Me.radYork.Name = "radYork"
-        Me.radYork.Size = New System.Drawing.Size(104, 24)
-        Me.radYork.TabIndex = 9
-        Me.radYork.TabStop = True
-        Me.radYork.Text = "York"
-        Me.radYork.UseVisualStyleBackColor = True
+        Me.pnlFormatControl.Controls.Add(Me.btnItalic)
+        Me.pnlFormatControl.Controls.Add(Me.btnUnderline)
+        Me.pnlFormatControl.Controls.Add(Me.btnBold)
+        Me.pnlFormatControl.Location = New System.Drawing.Point(576, 354)
+        Me.pnlFormatControl.Name = "pnlFormatControl"
+        Me.pnlFormatControl.Size = New System.Drawing.Size(160, 140)
+        Me.pnlFormatControl.TabIndex = 4
         '
-        'radCambridge
+        'btnItalic
         '
-        Me.radCambridge.Location = New System.Drawing.Point(4, 196)
-        Me.radCambridge.Name = "radCambridge"
-        Me.radCambridge.Size = New System.Drawing.Size(104, 24)
-        Me.radCambridge.TabIndex = 8
-        Me.radCambridge.TabStop = True
-        Me.radCambridge.Text = "Cambridge"
-        Me.radCambridge.UseVisualStyleBackColor = True
+        Me.btnItalic.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnItalic.Location = New System.Drawing.Point(36, 94)
+        Me.btnItalic.Name = "btnItalic"
+        Me.btnItalic.Size = New System.Drawing.Size(75, 23)
+        Me.btnItalic.TabIndex = 2
+        Me.btnItalic.Text = "Italic"
+        Me.btnItalic.UseVisualStyleBackColor = True
         '
-        'radLiverpoll
+        'btnUnderline
         '
-        Me.radLiverpoll.Location = New System.Drawing.Point(4, 173)
-        Me.radLiverpoll.Name = "radLiverpoll"
-        Me.radLiverpoll.Size = New System.Drawing.Size(104, 24)
-        Me.radLiverpoll.TabIndex = 7
-        Me.radLiverpoll.TabStop = True
-        Me.radLiverpoll.Text = "Liverpool"
-        Me.radLiverpoll.UseVisualStyleBackColor = True
+        Me.btnUnderline.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUnderline.Location = New System.Drawing.Point(36, 65)
+        Me.btnUnderline.Name = "btnUnderline"
+        Me.btnUnderline.Size = New System.Drawing.Size(75, 23)
+        Me.btnUnderline.TabIndex = 1
+        Me.btnUnderline.Text = "Underline"
+        Me.btnUnderline.UseVisualStyleBackColor = True
         '
-        'radBristol
+        'btnBold
         '
-        Me.radBristol.Location = New System.Drawing.Point(4, 148)
-        Me.radBristol.Name = "radBristol"
-        Me.radBristol.Size = New System.Drawing.Size(104, 24)
-        Me.radBristol.TabIndex = 6
-        Me.radBristol.TabStop = True
-        Me.radBristol.Text = "Bristol"
-        Me.radBristol.UseVisualStyleBackColor = True
+        Me.btnBold.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBold.Location = New System.Drawing.Point(36, 36)
+        Me.btnBold.Name = "btnBold"
+        Me.btnBold.Size = New System.Drawing.Size(75, 23)
+        Me.btnBold.TabIndex = 0
+        Me.btnBold.Text = "Bold"
+        Me.btnBold.UseVisualStyleBackColor = True
         '
-        'radManchester
+        'btnExit
         '
-        Me.radManchester.Location = New System.Drawing.Point(4, 124)
-        Me.radManchester.Name = "radManchester"
-        Me.radManchester.Size = New System.Drawing.Size(104, 24)
-        Me.radManchester.TabIndex = 5
-        Me.radManchester.TabStop = True
-        Me.radManchester.Text = "Mancherster"
-        Me.radManchester.UseVisualStyleBackColor = True
+        Me.btnExit.Location = New System.Drawing.Point(701, 526)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
-        'radQuezon
+        'lblDesignedby
         '
-        Me.radQuezon.Location = New System.Drawing.Point(4, 100)
-        Me.radQuezon.Name = "radQuezon"
-        Me.radQuezon.Size = New System.Drawing.Size(104, 24)
-        Me.radQuezon.TabIndex = 4
-        Me.radQuezon.TabStop = True
-        Me.radQuezon.Text = "Quezon City"
-        Me.radQuezon.UseVisualStyleBackColor = True
-        '
-        'Manila
-        '
-        Me.Manila.Location = New System.Drawing.Point(4, 76)
-        Me.Manila.Name = "Manila"
-        Me.Manila.Size = New System.Drawing.Size(104, 24)
-        Me.Manila.TabIndex = 3
-        Me.Manila.TabStop = True
-        Me.Manila.Text = "Manila"
-        Me.Manila.UseVisualStyleBackColor = True
-        '
-        'radIloilo
-        '
-        Me.radIloilo.Location = New System.Drawing.Point(4, 52)
-        Me.radIloilo.Name = "radIloilo"
-        Me.radIloilo.Size = New System.Drawing.Size(104, 24)
-        Me.radIloilo.TabIndex = 2
-        Me.radIloilo.TabStop = True
-        Me.radIloilo.Text = "Iloilo City"
-        Me.radIloilo.UseVisualStyleBackColor = True
-        '
-        'radBoracay
-        '
-        Me.radBoracay.Location = New System.Drawing.Point(4, 28)
-        Me.radBoracay.Name = "radBoracay"
-        Me.radBoracay.Size = New System.Drawing.Size(104, 24)
-        Me.radBoracay.TabIndex = 1
-        Me.radBoracay.TabStop = True
-        Me.radBoracay.Text = "Boracay"
-        Me.radBoracay.UseVisualStyleBackColor = True
-        '
-        'radBacolod
-        '
-        Me.radBacolod.Location = New System.Drawing.Point(4, 4)
-        Me.radBacolod.Name = "radBacolod"
-        Me.radBacolod.Size = New System.Drawing.Size(104, 24)
-        Me.radBacolod.TabIndex = 0
-        Me.radBacolod.TabStop = True
-        Me.radBacolod.Text = "Bacolod"
-        Me.radBacolod.UseVisualStyleBackColor = True
-        '
-        'picCity
-        '
-        Me.picCity.Location = New System.Drawing.Point(158, 12)
-        Me.picCity.Name = "picCity"
-        Me.picCity.Size = New System.Drawing.Size(448, 271)
-        Me.picCity.TabIndex = 4
-        Me.picCity.TabStop = False
+        Me.lblDesignedby.AutoSize = True
+        Me.lblDesignedby.Location = New System.Drawing.Point(10, 526)
+        Me.lblDesignedby.Name = "lblDesignedby"
+        Me.lblDesignedby.Size = New System.Drawing.Size(277, 13)
+        Me.lblDesignedby.TabIndex = 6
+        Me.lblDesignedby.Text = "Louie Escalera,Linda Lim,Elizabeth Robinson,Neil Wiborg"
         '
         'frmCities
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.ClientSize = New System.Drawing.Size(784, 561)
-        Me.Controls.Add(Me.picCity)
-        Me.Controls.Add(Me.pnlCities)
+        Me.Controls.Add(Me.lblDesignedby)
         Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.pnlFormatControl)
+        Me.Controls.Add(Me.pnlCities)
+        Me.Controls.Add(Me.lstCountries)
         Me.Controls.Add(Me.rtbInformation)
-        Me.Controls.Add(Me.lblWrittenBy)
-        Me.Controls.Add(Me.pnlFontStyle)
+        Me.Controls.Add(Me.picCities)
         Me.Name = "frmCities"
-        Me.Text = "World Cities: Philippines and United Kindgom"
-        Me.pnlFontStyle.ResumeLayout(False)
-        Me.pnlCities.ResumeLayout(False)
-        CType(Me.picCity, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "World Cities"
+        CType(Me.picCities, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlFormatControl.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents pnlFontStyle As Panel
-    Friend WithEvents btnExit As Button
-    Friend WithEvents chkBold As CheckBox
-    Friend WithEvents chkItalic As CheckBox
-    Friend WithEvents btnReset As Button
-    Friend WithEvents lblWrittenBy As Label
+    Friend WithEvents picCities As PictureBox
     Friend WithEvents rtbInformation As RichTextBox
+    Friend WithEvents lstCountries As ListBox
     Friend WithEvents pnlCities As Panel
-    Friend WithEvents picCity As PictureBox
-    Friend WithEvents radYork As RadioButton
-    Friend WithEvents radCambridge As RadioButton
-    Friend WithEvents radLiverpoll As RadioButton
-    Friend WithEvents radBristol As RadioButton
-    Friend WithEvents radManchester As RadioButton
-    Friend WithEvents radQuezon As RadioButton
-    Friend WithEvents Manila As RadioButton
-    Friend WithEvents radIloilo As RadioButton
-    Friend WithEvents radBoracay As RadioButton
-    Friend WithEvents radBacolod As RadioButton
+    Friend WithEvents pnlFormatControl As Panel
+    Friend WithEvents btnItalic As Button
+    Friend WithEvents btnUnderline As Button
+    Friend WithEvents btnBold As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDesignedby As Label
 End Class
--- WorldCitiesProject/frmCities.vb
+++ WorldCitiesProject/frmCities.vb
@@ -1,3 +1,76 @@
 ﻿Public Class frmCities
 
+
+    ''CountryArray(i,p) i = hold the country placement
+    ''p = the data
+    ''Country(i,0) holds line count
+    ''country(i,1)  holds city name
+    ''country(i,2) hold country where city is from
+    ''country(i,3) holds population
+    ''country(i,4) holds countryAnnualRainfall
+    ''Country(i,5) holds Image Name
+    ''Country(i,6+) hold description
+    Dim strCitiesFromRussia(,) As String
+    Dim strCitiesFromPhilippines(4, 80) As String
+    Dim strCitiesFromUK(,) As String
+
+    Private Sub frmCities_Load(sender As Object,
+                               e As EventArgs) Handles MyBase.Load
+        Dim fileCities As String() = System.IO.Directory.GetFiles("../Data",
+                                                                  "*.wcf")
+        Dim inttempFileLines As Integer
+        Dim strtempCityName As String = ""
+        Dim strCountryName As String = ""
+        Dim strTemp As String = ""
+        Dim intPhilippinesCountArray As Integer = 0
+        Dim intRussiaCountArray As Integer = 0
+        Dim intUKCountArray As Integer = 0
+
+
+
+        For Each sFile As String In fileCities
+            FileOpen(1, sFile, OpenMode.Input)
+            Input(1, inttempFileLines)
+            Input(1, strtempCityName)
+            Input(1, strTemp)
+            strCountryName = strTemp
+            FileClose(1)
+            Select Case strCountryName
+                Case "Philippines"
+                    FileOpen(1, sFile, OpenMode.Input)
+                    Input(1, strTemp)
+                    Input(1, strTemp)
+                    Input(1, strTemp)
+                    Input(1, strTemp)
+                    strCitiesFromPhilippines(intPhilippinesCountArray, 0) =
+                        CStr(inttempFileLines)
+                    strCitiesFromPhilippines(intPhilippinesCountArray, 1) =
+                        strtempCityName
+                    strCitiesFromPhilippines(intPhilippinesCountArray, 2) =
+                        strCountryName
+                    Input(1, strTemp)
+                    Input(1, strCitiesFromPhilippines(intPhilippinesCountArray,
+                                                      3))
+                    Input(1, strCitiesFromPhilippines(intPhilippinesCountArray,
+                                                      4))
+                    Dim i As Integer = 0
+                    Dim p As Integer = 5
+                    Dim intDescriptionSize = inttempFileLines - 1
+                    For i = 8 To intDescriptionSize
+                        Input(1, strTemp)
+                        strCitiesFromPhilippines(intPhilippinesCountArray, p) =
+                            strTemp
+                        p += 1
+                    Next i
+                    Input(1, strTemp)
+                    Input(1, strTemp)
+                    strCitiesFromPhilippines(intPhilippinesCountArray, 5) =
+                        strTemp
+                    intPhilippinesCountArray += 1
+                    FileClose(1)
+            End Select
+
+        Next
+
+    End Sub
 End Class
