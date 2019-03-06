5bfe707725fa030d3fe85f1102baa61130b24aea
Beasting <louie.escalera@students.aie.edu.au>
2019-03-05 22:56:40 -0800

    add reset
    forgot reset button so i added it in

--- WorldCitiesProject/frmCities.Designer.vb
+++ WorldCitiesProject/frmCities.Designer.vb
@@ -26,19 +26,18 @@ Partial Class frmCities
         Me.rtbInformation = New System.Windows.Forms.RichTextBox()
         Me.lstCountries = New System.Windows.Forms.ListBox()
         Me.pnlCities = New System.Windows.Forms.Panel()
-        Me.pnlFormatControl = New System.Windows.Forms.Panel()
-        Me.btnItalic = New System.Windows.Forms.Button()
-        Me.btnUnderline = New System.Windows.Forms.Button()
-        Me.btnBold = New System.Windows.Forms.Button()
+        Me.radCity4 = New System.Windows.Forms.RadioButton()
+        Me.radCity3 = New System.Windows.Forms.RadioButton()
+        Me.radCity2 = New System.Windows.Forms.RadioButton()
+        Me.radCity1 = New System.Windows.Forms.RadioButton()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.lblDesignedby = New System.Windows.Forms.Label()
-        Me.radCity1 = New System.Windows.Forms.RadioButton()
-        Me.radCity2 = New System.Windows.Forms.RadioButton()
-        Me.radCity3 = New System.Windows.Forms.RadioButton()
-        Me.radCity4 = New System.Windows.Forms.RadioButton()
+        Me.btnBold = New System.Windows.Forms.Button()
+        Me.btnUnderline = New System.Windows.Forms.Button()
+        Me.btnItalic = New System.Windows.Forms.Button()
+        Me.btnRest = New System.Windows.Forms.Button()
         CType(Me.picCities, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.pnlCities.SuspendLayout()
-        Me.pnlFormatControl.SuspendLayout()
         Me.SuspendLayout()
         '
         'picCities
@@ -77,49 +76,49 @@ Partial Class frmCities
         Me.pnlCities.Size = New System.Drawing.Size(200, 160)
         Me.pnlCities.TabIndex = 3
         '
-        'pnlFormatControl
+        'radCity4
         '
-        Me.pnlFormatControl.Controls.Add(Me.btnItalic)
-        Me.pnlFormatControl.Controls.Add(Me.btnUnderline)
-        Me.pnlFormatControl.Controls.Add(Me.btnBold)
-        Me.pnlFormatControl.Location = New System.Drawing.Point(576, 354)
-        Me.pnlFormatControl.Name = "pnlFormatControl"
-        Me.pnlFormatControl.Size = New System.Drawing.Size(160, 140)
-        Me.pnlFormatControl.TabIndex = 4
+        Me.radCity4.Location = New System.Drawing.Point(4, 94)
+        Me.radCity4.Name = "radCity4"
+        Me.radCity4.Size = New System.Drawing.Size(104, 24)
+        Me.radCity4.TabIndex = 3
+        Me.radCity4.TabStop = True
+        Me.radCity4.Text = "City4"
+        Me.radCity4.UseVisualStyleBackColor = True
         '
-        'btnItalic
+        'radCity3
         '
-        Me.btnItalic.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnItalic.Location = New System.Drawing.Point(36, 94)
-        Me.btnItalic.Name = "btnItalic"
-        Me.btnItalic.Size = New System.Drawing.Size(75, 23)
-        Me.btnItalic.TabIndex = 2
-        Me.btnItalic.Text = "Italic"
-        Me.btnItalic.UseVisualStyleBackColor = True
+        Me.radCity3.Location = New System.Drawing.Point(4, 70)
+        Me.radCity3.Name = "radCity3"
+        Me.radCity3.Size = New System.Drawing.Size(104, 24)
+        Me.radCity3.TabIndex = 2
+        Me.radCity3.TabStop = True
+        Me.radCity3.Text = "City3"
+        Me.radCity3.UseVisualStyleBackColor = True
         '
-        'btnUnderline
+        'radCity2
         '
-        Me.btnUnderline.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnUnderline.Location = New System.Drawing.Point(36, 65)
-        Me.btnUnderline.Name = "btnUnderline"
-        Me.btnUnderline.Size = New System.Drawing.Size(75, 23)
-        Me.btnUnderline.TabIndex = 1
-        Me.btnUnderline.Text = "Underline"
-        Me.btnUnderline.UseVisualStyleBackColor = True
+        Me.radCity2.Location = New System.Drawing.Point(4, 46)
+        Me.radCity2.Name = "radCity2"
+        Me.radCity2.Size = New System.Drawing.Size(104, 24)
+        Me.radCity2.TabIndex = 1
+        Me.radCity2.TabStop = True
+        Me.radCity2.Text = "City2"
+        Me.radCity2.UseVisualStyleBackColor = True
         '
-        'btnBold
+        'radCity1
         '
-        Me.btnBold.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBold.Location = New System.Drawing.Point(36, 36)
-        Me.btnBold.Name = "btnBold"
-        Me.btnBold.Size = New System.Drawing.Size(75, 23)
-        Me.btnBold.TabIndex = 0
-        Me.btnBold.Text = "Bold"
-        Me.btnBold.UseVisualStyleBackColor = True
+        Me.radCity1.Location = New System.Drawing.Point(4, 22)
+        Me.radCity1.Name = "radCity1"
+        Me.radCity1.Size = New System.Drawing.Size(104, 24)
+        Me.radCity1.TabIndex = 0
+        Me.radCity1.TabStop = True
+        Me.radCity1.Text = "City1"
+        Me.radCity1.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(701, 526)
+        Me.btnExit.Location = New System.Drawing.Point(701, 516)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(75, 23)
         Me.btnExit.TabIndex = 5
@@ -135,53 +134,55 @@ Partial Class frmCities
         Me.lblDesignedby.TabIndex = 6
         Me.lblDesignedby.Text = "Louie Escalera,Linda Lim,Elizabeth Robinson,Neil Wiborg"
         '
-        'radCity1
+        'btnBold
         '
-        Me.radCity1.Location = New System.Drawing.Point(4, 22)
-        Me.radCity1.Name = "radCity1"
-        Me.radCity1.Size = New System.Drawing.Size(104, 24)
-        Me.radCity1.TabIndex = 0
-        Me.radCity1.TabStop = True
-        Me.radCity1.Text = "City1"
-        Me.radCity1.UseVisualStyleBackColor = True
+        Me.btnBold.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBold.Location = New System.Drawing.Point(576, 407)
+        Me.btnBold.Name = "btnBold"
+        Me.btnBold.Size = New System.Drawing.Size(75, 23)
+        Me.btnBold.TabIndex = 0
+        Me.btnBold.Text = "Bold"
+        Me.btnBold.UseVisualStyleBackColor = True
         '
-        'radCity2
+        'btnUnderline
         '
-        Me.radCity2.Location = New System.Drawing.Point(4, 46)
-        Me.radCity2.Name = "radCity2"
-        Me.radCity2.Size = New System.Drawing.Size(104, 24)
-        Me.radCity2.TabIndex = 1
-        Me.radCity2.TabStop = True
-        Me.radCity2.Text = "City2"
-        Me.radCity2.UseVisualStyleBackColor = True
+        Me.btnUnderline.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUnderline.Location = New System.Drawing.Point(576, 436)
+        Me.btnUnderline.Name = "btnUnderline"
+        Me.btnUnderline.Size = New System.Drawing.Size(75, 23)
+        Me.btnUnderline.TabIndex = 1
+        Me.btnUnderline.Text = "Underline"
+        Me.btnUnderline.UseVisualStyleBackColor = True
         '
-        'radCity3
+        'btnItalic
         '
-        Me.radCity3.Location = New System.Drawing.Point(4, 70)
-        Me.radCity3.Name = "radCity3"
-        Me.radCity3.Size = New System.Drawing.Size(104, 24)
-        Me.radCity3.TabIndex = 2
-        Me.radCity3.TabStop = True
-        Me.radCity3.Text = "City3"
-        Me.radCity3.UseVisualStyleBackColor = True
+        Me.btnItalic.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnItalic.Location = New System.Drawing.Point(576, 378)
+        Me.btnItalic.Name = "btnItalic"
+        Me.btnItalic.Size = New System.Drawing.Size(75, 23)
+        Me.btnItalic.TabIndex = 2
+        Me.btnItalic.Text = "Italic"
+        Me.btnItalic.UseVisualStyleBackColor = True
         '
-        'radCity4
+        'btnRest
         '
-        Me.radCity4.Location = New System.Drawing.Point(4, 94)
-        Me.radCity4.Name = "radCity4"
-        Me.radCity4.Size = New System.Drawing.Size(104, 24)
-        Me.radCity4.TabIndex = 3
-        Me.radCity4.TabStop = True
-        Me.radCity4.Text = "City4"
-        Me.radCity4.UseVisualStyleBackColor = True
+        Me.btnRest.Location = New System.Drawing.Point(701, 487)
+        Me.btnRest.Name = "btnRest"
+        Me.btnRest.Size = New System.Drawing.Size(75, 23)
+        Me.btnRest.TabIndex = 7
+        Me.btnRest.Text = "Reset"
+        Me.btnRest.UseVisualStyleBackColor = True
         '
         'frmCities
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.btnRest)
+        Me.Controls.Add(Me.btnItalic)
         Me.Controls.Add(Me.lblDesignedby)
+        Me.Controls.Add(Me.btnUnderline)
         Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.pnlFormatControl)
+        Me.Controls.Add(Me.btnBold)
         Me.Controls.Add(Me.pnlCities)
         Me.Controls.Add(Me.lstCountries)
         Me.Controls.Add(Me.rtbInformation)
@@ -190,7 +191,6 @@ Partial Class frmCities
         Me.Text = "World Cities"
         CType(Me.picCities, System.ComponentModel.ISupportInitialize).EndInit()
         Me.pnlCities.ResumeLayout(False)
-        Me.pnlFormatControl.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -200,14 +200,14 @@ Partial Class frmCities
     Friend WithEvents rtbInformation As RichTextBox
     Friend WithEvents lstCountries As ListBox
     Friend WithEvents pnlCities As Panel
-    Friend WithEvents pnlFormatControl As Panel
-    Friend WithEvents btnItalic As Button
-    Friend WithEvents btnUnderline As Button
-    Friend WithEvents btnBold As Button
     Friend WithEvents btnExit As Button
     Friend WithEvents lblDesignedby As Label
     Friend WithEvents radCity4 As RadioButton
     Friend WithEvents radCity3 As RadioButton
     Friend WithEvents radCity2 As RadioButton
     Friend WithEvents radCity1 As RadioButton
+    Friend WithEvents btnBold As Button
+    Friend WithEvents btnUnderline As Button
+    Friend WithEvents btnItalic As Button
+    Friend WithEvents btnRest As Button
 End Class
